cebe8db5718e04d6d0df5cd111ade0f5ca1a43ba
이화원 <andylhw12@soongsil.ac.kr>
2021-05-09 19:51:55 +0900

    added MainClient

---
+++ src/Client/DynamicBeat.java
@@ -0,0 +1,14 @@
+package Client;
+
+import javax.swing.*;
+
+public class DynamicBeat extends JFrame{
+    public DynamicBeat(){
+        setTitle("Dynamic Beat");
+        setSize(MainClient.SC_WIDTH, MainClient.SC_HEIGHT);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
---
+++ src/Client/MainClient.java
@@ -0,0 +1,10 @@
+package Client;
+
+public class MainClient {
+    public static final int SC_WIDTH = 1280;
+    public static final int SC_HEIGHT = 720;
+
+    public static void main(String[] args){
+        new DynamicBeat();
+    }
+}
